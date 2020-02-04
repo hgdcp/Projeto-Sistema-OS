0e5e8257698aca26222a53ce5ddcc97a2854ef0d
Henrique Patrao <henrique.gdcp@gmail.com>
2020-02-03 22:14:11 -0300

    Primeiro Commit

---
+++ prjinfoX/src/br/com/infox/dal/ModuloConexao.java
@@ -0,0 +1,25 @@
+package br.com.infox.dal;
+
+import java.sql.*;
+
+public class ModuloConexao {
+
+    //método responsável por estabelecer a conexão com o banco
+    public static Connection conector() {
+        java.sql.Connection conexao = null;
+        // a linha abaixo "chama" o driver
+        String driver = "com.mysql.jdbc.Driver";
+        // armazenando informações referentes ao banco
+        String url = "jdbc:mysql://localhost:3307/dbinfox";
+        String user = "root";
+        String password = "";
+        // estabelecendo a conexão com o banco
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
